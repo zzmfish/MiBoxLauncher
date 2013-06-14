202c971566d2cd781067d24820d6ae60c6193572
zhouzm <zzm_guitar@163.com>
2013-06-14 15:06:40 +0800

    save applications info to a list

--- src/org/zzmfish/MiBoxLauncher/Home.java
+++ src/org/zzmfish/MiBoxLauncher/Home.java
@@ -1,18 +1,20 @@
 package org.zzmfish.MiBoxLauncher;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Home extends Activity
 {
-    /** Called when the activity is first created. */
     private static final String TAG = "MiBoxLauncher";
+    private static ArrayList<ApplicationInfo> mApplications;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -34,11 +36,28 @@ public class Home extends Activity
 
         if (apps != null) {
             final int count = apps.size();
+
+            if (mApplications == null) {
+                mApplications = new ArrayList<ApplicationInfo>(count);
+            }
+            mApplications.clear();
+
             for (int i = 0; i < count; i++) {
+                ApplicationInfo application = new ApplicationInfo();
                 ResolveInfo info = apps.get(i);
                 Log.d(TAG, "label=" + info.loadLabel(manager));
+
+                application.title = info.loadLabel(manager);
+                application.setActivity(new ComponentName(
+                        info.activityInfo.applicationInfo.packageName,
+                        info.activityInfo.name),
+                        Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                application.icon = info.activityInfo.loadIcon(manager);
+
+                mApplications.add(application);
+
             }
         }
-
     }
 }
