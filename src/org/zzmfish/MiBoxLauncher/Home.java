d2dfc628cadae9ab6cc1d478ef2b23b3ca63c179
zhouzm <zzm_guitar@163.com>
2013-11-06 07:35:33 +0800

    refactor

--- src/org/zzmfish/MiBoxLauncher/Home.java
+++ src/org/zzmfish/MiBoxLauncher/Home.java
@@ -1,26 +1,20 @@
 package org.zzmfish.MiBoxLauncher;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
-import android.widget.NumberPicker;
 
 import java.util.ArrayList;
 
@@ -111,8 +105,9 @@ public class Home extends Activity
 			uninstallApplication();
 			break;
 		case R.id.move:
-		    DialogFragment newFragment = new MoveAppDialog(mGrid.getSelectedItemPosition());
-		    newFragment.show(getFragmentManager(), "missiles");
+		    MoveAppDialog dialog = new MoveAppDialog();
+		    dialog.setFromIndex(mGrid.getSelectedItemPosition());
+		    dialog.show(getFragmentManager(), "missiles");
 		    loadApplications();
 			break;
 		case R.id.exit:
@@ -123,42 +118,4 @@ public class Home extends Activity
 	}
 }
 
-/**
- * 移动程序对话框
- */
-class MoveAppDialog extends DialogFragment {
-	int mFromIndex;
-	
-	public MoveAppDialog(int fromIndex) {
-		super();
-		mFromIndex = fromIndex;
-	}
-	
-	public int GetFromIndex() {
-		return mFromIndex;
-	}
 
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.move_app, null);
-        builder.setView(view);
-        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-    		@Override
-    		public void onClick(DialogInterface dialog, int id) {
-    			Dialog myDialog = MoveAppDialog.this.getDialog();
-    			int toIndex = ((NumberPicker) myDialog.findViewById(R.id.app_pos)).getValue();
-    			AppList.getInstance().move(MoveAppDialog.this.GetFromIndex(), toIndex);
-    		}
-    	});
-        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-    		@Override
-    		public void onClick(DialogInterface dialog, int id) {
-    		}
-    	});
-        Dialog dialog = builder.create();
-        return dialog;
-	}
-	
-}
